b81f0f1d5288d4ef171d402f3799341bb7f1c2ed
Joao vitor <vitorskyline17@gmail.com>
2023-02-09 12:02:50 -0300

    finished my fist project node

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const port = 3000
+const uuid = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+
+
+/*const myFistMiddleware = (request, response, next) => {
+
+    console.log("Fui chamado")
+    
+    next()
+
+    console.log("Finalizamos")
+}*/
+
+const users = []
+
+
+
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ error: "user not found"})
+    }
+
+    request.userIndex = index
+    request.useId = id
+    next ()
+}
+
+
+
+
+app.get('/users', (request, response) => {
+
+    return response.json(users)
+})
+
+
+app.post('/users', (request, response) => {
+    const {name, age} = request.body
+
+    const user = {id: uuid.v4(), name, age}
+
+    users.push(user)
+
+    return response.status(201).json(user)
+}) 
+
+
+app.put('/users/:id', checkUserId, (request, response) => {
+
+    const {name, age} = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updateUser = {id, name, age}
+
+    users [index] = updateUser
+
+    return response.json(updateUser)
+})
+
+app.delete('/users/:id', (request, response) => {
+
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({message: "user not found"})
+    }
+
+    users.splice(index,1)
+
+    return response.status(204).json(users)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`🚀 server started on port ${port}`)
+})
